86632de7d3cb0ded992ad9bf455fb174423671ec
XSigma Dev <dev@xsigma.co.uk>
2026-05-14 14:30:44 +0100

    update tensor

--- common/data_ptr.h
+++ common/data_ptr.h
@@ -1,14 +1,15 @@
 #pragma once
 
 #include <cstddef>
+#include <cstdint>
 
-#include "common/memory_macros.h"
 #include "allocator.h"
 #include "common/device.h"
+#include "common/memory_macros.h"
 
 namespace memory
 {
-template <typename value_t, bool deepcopy>
+template <typename value_t, bool clone>
 struct data_ptr
 {
     using allocator_t = allocator<value_t>;
@@ -16,52 +17,62 @@ struct data_ptr
     MEMORY_FORCE_INLINE data_ptr() = default;
 
     MEMORY_FORCE_INLINE data_ptr(size_t size, device_enum type)
-        : data_(allocator_t::allocate(size, type)), size_(size), type_(type), allocated_(true)
+        : data_(allocator_t::allocate(size, type)),
+          size_(size),
+          type_(type),
+          allocated_(true),
+          aligned_(true)  // allocator always satisfies MEMORY_ALIGNMENT
     {
     }
 
     MEMORY_FORCE_INLINE data_ptr(value_t* data, size_t size, device_enum type)
-        : size_(size), type_(type), allocated_(deepcopy)
+        : size_(size), type_(type), allocated_(clone)
     {
-        if constexpr (deepcopy)
+        if constexpr (clone)
         {
-            data_ = allocator_t::allocate(size, type),
+            data_ = allocator_t::allocate(size, type), aligned_ = true;
             allocator_t::copy(data, size_, data_, type, type_);
         }
         else
         {
-            data_ = data;
+            data_    = data;
+            aligned_ = is_ptr_aligned(data);
         }
     }
 
     MEMORY_FORCE_INLINE data_ptr(
         value_t* data, size_t size, device_enum from_type, device_enum to_type)
-        : size_(size), type_(to_type), allocated_(deepcopy)
+        : size_(size), type_(to_type), allocated_(clone)
     {
-        if constexpr (deepcopy)
+        if constexpr (clone)
         {
-            data_ = allocator_t::allocate(size, to_type),
+            data_    = allocator_t::allocate(size, to_type);
+            aligned_ = true;
             allocator_t::copy(data, size_, data_, from_type, to_type);
         }
         else
         {
-            data_ = data;
+            data_    = data;
+            aligned_ = is_ptr_aligned(data);
         }
     }
 
+    // Copy — behaviour controlled by clone template parameter
     MEMORY_FORCE_INLINE data_ptr(data_ptr const& rhs)
-        : size_(rhs.size_), type_(rhs.type_), allocated_(deepcopy)
+        : size_(rhs.size_), type_(rhs.type_), allocated_(clone)
     {
-        if constexpr (deepcopy)
+        if constexpr (clone)
         {
-            data_ = allocator_t::allocate(size_, type_),
+            data_    = allocator_t::allocate(size_, type_);
+            aligned_ = true;
             allocator_t::copy(rhs.data_, size_, data_, rhs.type_, type_);
         }
         else
         {
-            data_ = rhs.data_;
+            data_    = rhs.data_;
+            aligned_ = rhs.aligned_;
         }
-    };
+    }
 
     MEMORY_FORCE_INLINE data_ptr& operator=(data_ptr const& rhs)
     {
@@ -69,40 +80,55 @@ struct data_ptr
         {
             return *this;
         }
-        size_      = rhs.size_;
-        allocated_ = deepcopy;
 
-        if constexpr (deepcopy)
+        size_      = rhs.size_;
+        type_      = rhs.type_;
+        allocated_ = clone;
+
+        if constexpr (clone)
         {
-            data_ = allocator_t::allocate(size_, type_),
+            data_    = allocator_t::allocate(size_, type_);
+            aligned_ = true;
             allocator_t::copy(rhs.data_, size_, data_, rhs.type_, type_);
         }
         else
         {
-            data_ = rhs.data_;
-            type_ = rhs.type_;
+            data_    = rhs.data_;
+            aligned_ = rhs.aligned_;
         }
 
         return *this;
     }
 
-    MEMORY_FORCE_INLINE data_ptr(data_ptr&& rhs)
+    // Move — always transfers ownership, independent of clone
+    MEMORY_FORCE_INLINE data_ptr(data_ptr&& rhs) noexcept
         : data_(std::move(rhs.data_)),
           size_(std::move(rhs.size_)),
           type_(std::move(rhs.type_)),
-          allocated_(std::move(rhs.allocated_))
+          allocated_(std::move(rhs.allocated_)),
+          aligned_(std::move(rhs.aligned_))
     {
-        rhs.data_ = nullptr;
-    };
+        rhs.data_      = nullptr;
+        rhs.allocated_ = false;
+        rhs.aligned_   = false;
+    }
 
     MEMORY_FORCE_INLINE data_ptr& operator=(data_ptr&& rhs) noexcept
     {
+        if (this == &rhs)
+        {
+            return *this;
+        }
+
         data_      = std::move(rhs.data_);
         size_      = std::move(rhs.size_);
         type_      = std::move(rhs.type_);
         allocated_ = std::move(rhs.allocated_);
+        aligned_   = std::move(rhs.aligned_);
 
-        rhs.data_ = nullptr;
+        rhs.data_      = nullptr;
+        rhs.allocated_ = false;
+        rhs.aligned_   = false;
 
         return *this;
     }
@@ -124,6 +150,7 @@ struct data_ptr
             type_      = rhs.type_;
             size_      = rhs.size_;
             allocated_ = true;
+            aligned_   = true;
         }
         if (data_ != rhs.data_)
         {
@@ -142,10 +169,18 @@ struct data_ptr
     MEMORY_FORCE_INLINE value_t* end() { return data() + size_; }
 
     MEMORY_FORCE_INLINE size_t size() const { return size_; }
+    MEMORY_FORCE_INLINE bool   is_aligned() const { return aligned_; }
 
     value_t*    data_{nullptr};
     size_t      size_{0};
     device_enum type_{device_enum::CPU};
-    bool        allocated_;
+    bool        allocated_{false};
+    bool        aligned_{false};
+
+private:
+    static MEMORY_FORCE_INLINE bool is_ptr_aligned(value_t const* ptr) noexcept
+    {
+        return ptr != nullptr && (reinterpret_cast<uintptr_t>(ptr) % MEMORY_ALIGNMENT == 0);
+    }
 };
 }  // namespace memory
